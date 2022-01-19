4d367366baa5fd8d1020359d65070f71237fe56f
Jesus Cervantes <cervantes@ciencias.unam.mx>
2022-01-18 21:05:02 -0600

    Modificacion grande

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Users from './components/Users/Users'
+import Inventory from './components/Inventory/Inventory'
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Routes>
-        <Route exact path='/' element={<Home />} />
-          {/* <Route path="/" element={} />
-          <Route path="/" element={} /> */}
+        <Route exact path='/Users' element={<Users />} />
+        <Route exact path='/Inventory' element={<Inventory />} />
+        <Route exact path='/Orders' element={<Users />} />
+        <Route exact path='/Category' element={<Users />} />
+        <Route exact path='/Favorite' element={<Users />} />
+        <Route exact path='/SalesBanner' element={<Users />} />
       </Routes>
     </>
   );
