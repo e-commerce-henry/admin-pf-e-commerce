8a1e33626f18344e067a2f1f64acfbb7aab8e258
Jesus Cervantes <cervantes@ciencias.unam.mx>
2022-01-26 17:41:41 -0600

    set url

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,27 +2,27 @@ const axios = require("axios");
 
 export function getProducts() {
 	return async function (dispatch) {
-		const products = await axios("http://localhost:3001/products");
+		const products = await axios("http://proyecto-personal.online/products");
 		return dispatch({ type: "GET_PRODUCTS", payload: products.data });
 	};
 }
 
 export function createProduct(newProduct) {
 	return async function () {
-		return await axios.post("http://localhost:3001/products", newProduct);
+		return await axios.post("http://proyecto-personal.online/products", newProduct);
 	};
 }
 
 export function getCategorys() {
 	return async function (dispatch) {
-		const categorys = await axios("http://localhost:3001/category");
+		const categorys = await axios("http://proyecto-personal.online/category");
 		return dispatch({ type: "GET_CATEGORYS", payload: categorys.data });
 	};
 }
 
 export function editProduct(id, value) {
 	return dispatch => {
-		axios.put(`http://localhost:3001/products/${id}`, value)
+		axios.put(`http://proyecto-personal.online/products/${id}`, value)
 			.then((result) => {
 				return dispatch({
 					type: "UPDATE_PRODUCT",
@@ -38,13 +38,13 @@ export function editProduct(id, value) {
 
 export function createCategory(newCategory) {
 	return async function () {
-		return await axios.post("http://localhost:3001/category", newCategory);
+		return await axios.post("http://proyecto-personal.online/category", newCategory);
 	};
 }
 
 export function getSaleBanner() {
 	return async function (dispatch) {
-		const saleBanner = await axios.get("http://localhost:3001/saleBanner");
+		const saleBanner = await axios.get("http://proyecto-personal.online/saleBanner");
 		return dispatch({ type: "GET_SALEBANNER", payload: saleBanner.data });
 	};
 }
@@ -52,7 +52,7 @@ export function getSaleBanner() {
 export function postSaleBanner(saleItem) {
 	return async function (dispatch) {
 		const newSaleItem = await axios.post(
-			"http://localhost:3001/saleBanner",
+			"http://proyecto-personal.online/saleBanner",
 			saleItem
 		);
 		return dispatch({ type: "POST_SALEBANNER", payload: newSaleItem.data });
@@ -63,7 +63,7 @@ export function postSaleBanner(saleItem) {
 export function deleteSaleBanner(saleItemId) {
 	return async function (dispatch) {
 		const removedItem = await axios.delete(
-			`http://localhost:3001/saleBanner/${saleItemId}`
+			`http://proyecto-personal.online/saleBanner/${saleItemId}`
 		);
 		return dispatch({ type: "DELETE_SALEBANNER", payload: saleItemId });
 	};
@@ -72,7 +72,7 @@ export function deleteSaleBanner(saleItemId) {
 
 export function authUser({email, pwd}) {
 	return async function (dispatch) {
-		let respuesta = await axios.post('http://localhost:3001/auth/signIn', {email, pwd}, { withCredentials: true })
+		let respuesta = await axios.post('http://proyecto-personal.online/auth/signIn', {email, pwd}, { withCredentials: true })
 		console.log('llegue aqui')
 		return (
 			dispatch({ type: "AUTH_USER", payload: respuesta })
