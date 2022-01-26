c7adfbc01d9fab04efe01c688b11dbba67f77ac0
Jesus Cervantes <cervantes@ciencias.unam.mx>
2022-01-26 12:23:13 -0600

    cors

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -15,18 +15,18 @@ export function createProduct(newProduct){
     }
 };
 
-export function editProduct(id,value){
-    return dispatch => {
-        axios.put(`http://localhost:3001/products/${id}`,value)
-        .then((result) => {
-            return dispatch({
-                type:"UPDATE_PRODUCT",
-                payload:result.data
-            })
-        }).catch((err) => {
-            console.error(err)
-        });
-    }
+export function editProduct(id, value) {
+	return dispatch => {
+		axios.put(`http://localhost:3001/products/${id}`, value)
+			.then((result) => {
+				return dispatch({
+					type: "UPDATE_PRODUCT",
+					payload: result.data
+				})
+			}).catch((err) => {
+				console.error(err)
+			});
+	}
 }
 
 export function getCategorys(){
@@ -38,9 +38,44 @@ export function getCategorys(){
     }
 };
 
-export function createCategory(newCategory){
-    return async function(){
-        return await axios.post('http://localhost:3001/category', newCategory)
-    }
-};
+export function createCategory(newCategory) {
+	return async function () {
+		return await axios.post("http://localhost:3001/category", newCategory);
+	};
+}
 
+export function getSaleBanner() {
+	return async function (dispatch) {
+		const saleBanner = await axios.get("http://localhost:3001/saleBanner");
+		return dispatch({ type: "GET_SALEBANNER", payload: saleBanner.data });
+	};
+}
+
+export function postSaleBanner(saleItem) {
+	return async function (dispatch) {
+		const newSaleItem = await axios.post(
+			"http://localhost:3001/saleBanner",
+			saleItem
+		);
+		return dispatch({ type: "POST_SALEBANNER", payload: newSaleItem.data });
+	};
+}
+
+export function deleteSaleBanner(saleItemId) {
+	return async function (dispatch) {
+		const removedItem = await axios.delete(
+			`http://localhost:3001/saleBanner/${saleItemId}`
+		);
+		return dispatch({ type: "DELETE_SALEBANNER", payload: saleItemId });
+	};
+}
+
+export function authUser({email, pwd}) {
+	return async function (dispatch) {
+		let respuesta = await axios.post('http://localhost:3001/auth/signIn', {email, pwd}, { withCredentials: true })
+		console.log('llegue aqui')
+		return (
+			dispatch({ type: "AUTH_USER", payload: respuesta })
+		)
+	}
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,30 +1,49 @@
 import {} from './actions';
 
 const inicialState = {
-    products: [],
-    categorys: [],
-    // updateproducts:[]
+	products: [],
+	categorys: [],
+	saleBanner: [],
+	authUser: []
 };
 
 const reducer = (state = inicialState, action) => {
-    switch (action.type) {
-        case "GET_CATEGORYS":
-            return {
-                ...state,
-                categorys: action.payload
-            }
-        case "GET_PRODUCTS":
-            return{
-                ...state,
-                products: action.payload
-            }
-        // case "UPDATE_PRODUCT":
-        //     return{
-        //         ...state,
-        //         updateproducts: action.payload
-        //     }
-    
-    default: return state 
-}
-}
-export default reducer; 
+	switch (action.type) {
+		case "GET_CATEGORYS":
+			return {
+				...state,
+				categorys: action.payload,
+			};
+		case "GET_PRODUCTS":
+			return {
+				...state,
+				products: action.payload.sort((a, b) => {
+					return a.id - b.id;
+				}),
+			};
+		case "GET_SALEBANNER":
+			return {
+				...state,
+				saleBanner: action.payload,
+			};
+		case "POST_SALEBANNER":
+			return {
+				...state,
+				saleBanner: [...state.saleBanner, action.payload],
+			};
+		case "DELETE_SALEBANNER":
+			console.log(action.payload);
+			return {
+				...state,
+				saleBanner: state.saleBanner.filter((e) => e.id != action.payload),
+			};
+		case "AUTH_USER":
+			return{
+				...state,
+				authUser: action.payload
+			}
+		default:
+			return state;
+	}
+};
+export default reducer;
