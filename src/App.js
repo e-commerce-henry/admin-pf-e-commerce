8e68158d449d003d23fed0dc43f78803f78ad671
Marco Monti <88577526+MontiMarco92@users.noreply.github.com>
2022-02-01 10:45:05 -0300

    changed JWT storage from cookies to localStorage (#23)

--- src/App.js
+++ src/App.js
@@ -16,20 +16,19 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<Auth />} />
-				{/* <Route path="/" element={<PublicRoute />}>
-					
-				</Route> */}
-				{/* <Route element={<RequireAuth />}> */}
-				<Route path="/home" element={<Navbar />}>
-					<Route path="Users" element={<UsersPanel />} />
-					<Route path="Inventory" element={<Inventory />} />
-					<Route path="Orders" element={<Orders />} />
-					<Route path="Category" element={<Categorys />} />
-					<Route path="Favorite" element={<Favorite />} />
-					<Route path="SalesBanner" element={<SalesBanner />} />
+				<Route path="/" element={<PublicRoute />}>
+					<Route path="/" element={<Auth />} />
 				</Route>
-				{/* </Route> */}
+				<Route element={<RequireAuth />}>
+					<Route path="/home" element={<Navbar />}>
+						<Route path="Users" element={<UsersPanel />} />
+						<Route path="Inventory" element={<Inventory />} />
+						<Route path="Orders" element={<Orders />} />
+						<Route path="Category" element={<Categorys />} />
+						<Route path="Favorite" element={<Favorite />} />
+						<Route path="SalesBanner" element={<SalesBanner />} />
+					</Route>
+				</Route>
 			</Routes>
 		</>
 	);
--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,12 +1,9 @@
-import { useEffect } from "react";
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
-import Cookies from "js-cookie";
 
 const RequireAuth = () => {
 	const { auth, setAuth } = useAuth();
 	const location = useLocation();
-	console.log(auth);
 
 	return auth?.token ? (
 		<Outlet />
