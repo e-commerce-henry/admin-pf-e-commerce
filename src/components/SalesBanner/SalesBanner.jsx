f085d2bfb887c0aa66dd21bca399cbd3b4ab2b88
Marco Monti <monti.marco92@hotmail.com>
2022-02-09 13:53:44 -0300

    se valida fomulario de sales banner

--- src/components/SalesBanner/SalesBanner.jsx
+++ src/components/SalesBanner/SalesBanner.jsx
@@ -5,6 +5,20 @@ import { getProducts, postSaleBanner } from "../../redux/actions";
 import BannerItems from "./BannerProducts/BannerProducts";
 import Style from './SalesBanner.module.css'
 
+
+const validate = (newSaleItem)=>{
+    const errors = {};
+
+    if(!newSaleItem.productId){errors.productId = 'Product Name is required'}
+
+    if(!newSaleItem.discount){errors.discount = 'A discount value is required'}
+    else if(!/^[1-9][0-9]?$|^100$/g.test(newSaleItem.discount)){
+        errors.discount = 'Invalid Discount - only round numbers between 1-100 are valid'
+    }
+    return errors;
+}
+
+
 export default function SalesBanner(){
     const dispatch = useDispatch();
     const {auth} = useAuth()
@@ -13,6 +27,7 @@ export default function SalesBanner(){
         productId: '',
         discount: '',
     })
+    const [errors, setErrors]=useState('');
 
     useEffect(()=>{
         dispatch(getProducts())
@@ -23,22 +38,27 @@ export default function SalesBanner(){
            ...newSaleItem,
             [e.target.name] : e.target.value
         })
+        setErrors(validate({
+            ...newSaleItem,
+            [e.target.name] : e.target.value
+        }))
     };
    
 
     const submitDiscountedItem = (e) =>{
         e.preventDefault();
-        const saleItem ={
-            productId: newSaleItem.productId,
-            discount: newSaleItem.discount,
+        if(Object.keys(errors).length === 0){
+            const saleItem ={
+                productId: newSaleItem.productId,
+                discount: newSaleItem.discount,
+            }
+            dispatch(postSaleBanner(saleItem, auth));
+            setNewSaleItem({
+                discount: '',
+                productId: '',
+            })
+            document.getElementById("SalesForm").reset();
         }
-        dispatch(postSaleBanner(saleItem, auth));
-        setNewSaleItem({
-            discount: '',
-            productId: '',
-        })
-        
-        
     }
 
 
@@ -62,19 +82,22 @@ export default function SalesBanner(){
                             }): null
                         }
                     </select>
+                    {!errors.productId ? null : <span className={Style.error}>{errors.productId}</span>}
                 </div>
                 
                 <div>
                     <label type="text">Discount (%) </label>
-                    <input type='text' placeholder="insert a percentage" name='discount' value={newSaleItem.discount} onChange={onChange}/>
+                    <input type='text' placeholder="insert a percentage" required name='discount' value={newSaleItem.discount} onChange={onChange}/>
+                    {!errors.discount ? null : <span className={Style.error}>{errors.discount}</span>}
                 </div>
 
                 <div>
                     <button className={Style.agregarsalebanner} type='submit'>Agregar producto a SaleBanner </button>
                 </div>
-
-
-
+                { errors && Object.keys(errors).length > 0 
+                    ? <span className={Style.error}>No se puede enviar formulario vacio o con errores</span>
+                    : null
+                }
             </form>
             <div className={Style.saleItems}>
                 <h1>Sale Banner</h1>
